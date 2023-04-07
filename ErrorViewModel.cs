550f4755375aef82aa943761bddbb65dc6b8fb4b
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2023-04-07 10:39:21 -0400

    added model in Jokeapplication

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JokeApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokeApplication.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion{ get; set; }
+
+        public string JokeAnswer{ get; set; }
+
+        public Joke()
+        {
+                
+        }
+
+    }
+}
