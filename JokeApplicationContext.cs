31d7a700bae8bdcc987055aac59efbc0e2d80c6e
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2023-04-07 10:37:04 -0400

    Add files via upload

---
+++ JokeApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokeApplication.Models;
+
+namespace JokeApplication.Data
+{
+    public class JokeApplicationContext : DbContext
+    {
+        public JokeApplicationContext (DbContextOptions<JokeApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokeApplication.Models.Joke> Joke { get; set; } = default!;
+    }
+}
